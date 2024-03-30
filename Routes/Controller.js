9bacb352ece4d2abe01cbc0773ca03f46f954c88
Rakshith Acharya <rakshixh@gmail.com>
2024-03-30 17:36:21 +0530

    Data is not written into File Storage DB (data.json).

--- Routes/Controller.js
+++ Routes/Controller.js
@@ -1,95 +1,83 @@
 const express = require("express");
 const apiRoutes = express.Router();
 const fs = require("fs");
-const { get } = require("http");
 
 const dataPath = "./Database/data.json";
 
-//Write function
-const saveData = (data) => {
-  const stringifyData = JSON.stringify(data);
-  fs.writeFileSync(dataPath, stringifyData);
+// Store original data from data.json in memory cache
+let cachedData = { jobs: [] };
+
+const readDataFromFile = () => {
+  try {
+    const jsonData = fs.readFileSync(dataPath);
+    cachedData = JSON.parse(jsonData);
+  } catch (error) {
+    console.error("Error reading data from file:", error);
+  }
 };
 
-//Read function
-const getData = () => {
-  const jsonData = fs.readFileSync(dataPath);
-  return JSON.parse(jsonData);
-};
+readDataFromFile(); // Initial read on server start
 
-// reading the data
-apiRoutes.get("/read/jobs", (req, res) => {
-  fs.readFile(dataPath, "utf8", (err, data) => {
-    if (err) {
-      throw err;
-    }
-    res.send(JSON.parse(data));
-  });
+// Middleware to update cache before each API request
+apiRoutes.use((req, res, next) => {
+  readDataFromFile();
+  next();
 });
 
-// To get the data also with query param ?_limit
+// To get the data
 apiRoutes.get("/jobs", (req, res) => {
-  const jobs = getData();
-  const jobs_limit = getData().jobs;
+  const jobs = cachedData.jobs;
   const limit = req.query._limit;
 
   if (limit && !isNaN(limit)) {
-    const limitedJobs = jobs_limit.slice(0, parseInt(limit));
+    const limitedJobs = jobs.slice(0, parseInt(limit));
     res.send(limitedJobs);
   } else {
     res.send(jobs);
   }
 });
 
-//To upload the data
+// To upload the data
 apiRoutes.post("/jobs", (req, res) => {
   const newJob = req.body;
-  const existingData = getData();
-  const existingJobs = existingData.jobs;
+  const newJobId = Math.floor(100000 + Math.random() * 900000).toString();
+  newJob.id = newJobId;
 
-  const newJobId = Math.floor(100000 + Math.random() * 900000);
-  newJob.id = newJobId.toString();
-
-  existingJobs.push(newJob);
-  existingData.jobs = existingJobs;
-  saveData(existingData);
-  res.send({ success: true, msg: "Job Created Successfully!" });
+  // Temporarily add new job to cachedData
+  cachedData.jobs.push(newJob);
+  res.send({ success: true, msg: "Job Created Successfully!", job: newJob });
 });
 
 // To update the data
 apiRoutes.put("/jobs/:id", (req, res) => {
   const jobId = req.params.id;
-  const existingData = getData();
-  const existingJobs = existingData.jobs;
-
-  const index = existingJobs.findIndex((job) => job.id === jobId);
+  const index = cachedData.jobs.findIndex((job) => job.id === jobId);
   if (index !== -1) {
     const updatedJob = { ...req.body, id: jobId };
-    existingJobs[index] = updatedJob;
-
-    existingData.jobs = existingJobs;
-    saveData(existingData);
-    res.send(`Job with id ${jobId} has been updated`);
+    cachedData.jobs[index] = updatedJob;
+    res.send({
+      success: true,
+      msg: `Job with id ${jobId} has been updated`,
+      job: updatedJob,
+    });
   } else {
-    res.status(404).send("Job not found");
+    res.status(404).send({ success: true, msg: "Job not found!" });
   }
 });
 
-//To delete the data
+// To delete the data
 apiRoutes.delete("/jobs/:id", (req, res) => {
   const jobId = req.params.id;
-  const existingData = getData();
-  const existingJobs = existingData.jobs;
-
-  const index = existingJobs.findIndex((job) => job.id === jobId);
+  const index = cachedData.jobs.findIndex((job) => job.id === jobId);
   if (index !== -1) {
-    existingJobs.splice(index, 1);
-
-    existingData.jobs = existingJobs;
-    saveData(existingData);
-    res.send(`Job with id ${jobId} has been deleted`);
+    const deletedJob = cachedData.jobs.splice(index, 1)[0];
+    res.send({
+      success: true,
+      msg: `Job with id ${jobId} has been deleted`,
+      job: deletedJob,
+    });
   } else {
-    res.status(404).send("Job not found");
+    res.status(404).send({ success: false, msg: "Job not found!" });
   }
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 
 // create our express app
 const app = express();
